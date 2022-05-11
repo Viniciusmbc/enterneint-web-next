d891b461f1bd15f67821b30bf8a3af26c4b6f185
Viniciusmbc <viniciuscaceres@gmail.com>
2022-05-11 14:54:04 -0300

    adicionado trending

---
+++ components/Cards.js
@@ -0,0 +1,48 @@
+export default function Cards() {
+  return (
+    <div>
+      <div className="relative ml-4 mr-2 w-9/12 max-w-md flex-shrink-0 ">
+        <div className=" flex items-center right-0 absolute bg-darkBlue/50  w-8 h-8 rounded-full">
+          <svg
+            className=" mx-auto"
+            width="12"
+            height="14"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="m10.518.75.399 12.214-5.084-4.24-4.535 4.426L.75 1.036l9.768-.285Z"
+              stroke="#FFF"
+              strokeWidth="1.5"
+              fill="none"
+            />
+          </svg>
+        </div>
+
+        <div className=" bottom-10 left-4 absolute flex items-center">
+          <p className="text-white/75 text-xs">2019</p>
+          <div className=" bg-white rounded-full w-1 h-1 mx-2"></div>
+          <svg width="12" height="12" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M16.956 0H3.044A3.044 3.044 0 0 0 0 3.044v13.912A3.044 3.044 0 0 0 3.044 20h13.912A3.044 3.044 0 0 0 20 16.956V3.044A3.044 3.044 0 0 0 16.956 0ZM4 9H2V7h2v2Zm-2 2h2v2H2v-2Zm16-2h-2V7h2v2Zm-2 2h2v2h-2v-2Zm2-8.26V4h-2V2h1.26a.74.74 0 0 1 .74.74ZM2.74 2H4v2H2V2.74A.74.74 0 0 1 2.74 2ZM2 17.26V16h2v2H2.74a.74.74 0 0 1-.74-.74Zm16 0a.74.74 0 0 1-.74.74H16v-2h2v1.26Z"
+              fill="#FFFFFF"
+            />
+          </svg>
+          <p className=" text-white/75 ml-2 text-xs">Movies</p>
+          <div className=" bg-white rounded-full w-1 h-1 mx-2"></div>
+          <p className=" text-white/75 text-xs">PG</p>
+        </div>
+        <div>
+          {" "}
+          <p className="absolute left-4 bottom-4 text-white text-sm">
+            Beyond Earth
+          </p>
+        </div>
+        <img
+          className="rounded"
+          src="https://github.com/Viniciusmbc/enterneint-web-next/blob/main/public/thumbnails/beyond-earth/trending/large.jpg?raw=true"
+          alt="beyond-earth"
+        />
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import Head from "next/head";
 import Image from "next/image";
 import NestedLayout from "../components/NestedLayout";
+import Trending from "../components/Trending";
 
 export default function Home() {
   return (
     <>
       <Head></Head>
       <main className=" bg-darkBlue">
-        <div className="flex px-4">
+        <div className="flex p-4">
           <svg
             className="mr-4"
             width="32"
@@ -25,57 +26,12 @@ export default function Home() {
           />
         </div>
 
-        <h1 className="px-4 text-xl text-white">Trending</h1>
+        <h1 className="pb-4 pl-4 text-xl text-white">Trending</h1>
 
         <div className="flex  w-full overflow-x-auto">
-          <div className=" ml-4 relative">
-            <div className=" bottom-10 left-4 absolute flex items-center">
-              <p className="text-white  text-xs">2019</p>
-              <div className=" bg-white rounded-full w-1 h-1 mx-2"></div>
-              <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M16.956 0H3.044A3.044 3.044 0 0 0 0 3.044v13.912A3.044 3.044 0 0 0 3.044 20h13.912A3.044 3.044 0 0 0 20 16.956V3.044A3.044 3.044 0 0 0 16.956 0ZM4 9H2V7h2v2Zm-2 2h2v2H2v-2Zm16-2h-2V7h2v2Zm-2 2h2v2h-2v-2Zm2-8.26V4h-2V2h1.26a.74.74 0 0 1 .74.74ZM2.74 2H4v2H2V2.74A.74.74 0 0 1 2.74 2ZM2 17.26V16h2v2H2.74a.74.74 0 0 1-.74-.74Zm16 0a.74.74 0 0 1-.74.74H16v-2h2v1.26Z"
-                  fill="#FFFFFF"
-                />
-              </svg>
-              <p className=" text-white ml-2">Movies</p>
-              <div className=" bg-white rounded-full w-1 h-1 mx-2"></div>
-              <p className=" text-white">PG</p>
-            </div>
-            <div>
-              {" "}
-              <p className="absolute left-4 bottom-4 text-white text-sm">
-                Beyond Earth
-              </p>
-            </div>
-            <img
-              className="max-w-sm rounded"
-              src="https://github.com/Viniciusmbc/enterneint-web-next/blob/main/public/thumbnails/beyond-earth/trending/large.jpg?raw=true"
-              alt="beyond-earth"
-            />
-          </div>
-          <div className=" mx-4">
-            <img
-              className="max-w-sm rounded"
-              src="https://github.com/Viniciusmbc/enterneint-web-next/blob/main/public/thumbnails/beyond-earth/trending/large.jpg?raw=true"
-              alt="beyond-earth"
-            />
-          </div>
-          <div className=" mr-4">
-            <img
-              className="max-w-sm rounded"
-              src="https://github.com/Viniciusmbc/enterneint-web-next/blob/main/public/thumbnails/beyond-earth/trending/large.jpg?raw=true"
-              alt="beyond-earth"
-            />
-          </div>
-          <div>
-            <img
-              className="max-w-sm rounded"
-              src="https://github.com/Viniciusmbc/enterneint-web-next/blob/main/public/thumbnails/beyond-earth/trending/large.jpg?raw=true"
-              alt="beyond-earth"
-            />
-          </div>
+          <Trending />
         </div>
+        <h2 className="text-white text-xl my-6 ml-4">Recommended for you</h2>
       </main>
     </>
   );
