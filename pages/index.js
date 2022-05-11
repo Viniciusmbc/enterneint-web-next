74c21b5a4b9d4068d7a10b290448f2e8c064d56a
Viniciusmbc <viniciuscaceres@gmail.com>
2022-05-11 11:18:42 -0300

    adicionado estilos a imagem

--- pages/index.js
+++ pages/index.js
@@ -29,21 +29,24 @@ export default function Home() {
 
         <div className="flex  w-full overflow-x-auto">
           <div className=" ml-4 relative">
-            <div className=" bottom-4 left-4 absolute">
-              <p className=" mb-1  text-white  text-xs">2019</p>
-              <div className=" bg-red rounded-full w-1 h-1"></div>
-              <svg
-                className="mx-2"
-                width="20"
-                height="20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
+            <div className=" bottom-10 left-4 absolute flex items-center">
+              <p className="text-white  text-xs">2019</p>
+              <div className=" bg-white rounded-full w-1 h-1 mx-2"></div>
+              <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M16.956 0H3.044A3.044 3.044 0 0 0 0 3.044v13.912A3.044 3.044 0 0 0 3.044 20h13.912A3.044 3.044 0 0 0 20 16.956V3.044A3.044 3.044 0 0 0 16.956 0ZM4 9H2V7h2v2Zm-2 2h2v2H2v-2Zm16-2h-2V7h2v2Zm-2 2h2v2h-2v-2Zm2-8.26V4h-2V2h1.26a.74.74 0 0 1 .74.74ZM2.74 2H4v2H2V2.74A.74.74 0 0 1 2.74 2ZM2 17.26V16h2v2H2.74a.74.74 0 0 1-.74-.74Zm16 0a.74.74 0 0 1-.74.74H16v-2h2v1.26Z"
                   fill="#FFFFFF"
                 />
               </svg>
-              <p className="bottom-4 text-white text-sm">Beyond Earth</p>
+              <p className=" text-white ml-2">Movies</p>
+              <div className=" bg-white rounded-full w-1 h-1 mx-2"></div>
+              <p className=" text-white">PG</p>
+            </div>
+            <div>
+              {" "}
+              <p className="absolute left-4 bottom-4 text-white text-sm">
+                Beyond Earth
+              </p>
             </div>
             <img
               className="max-w-sm rounded"
@@ -67,7 +70,7 @@ export default function Home() {
           </div>
           <div>
             <img
-              className="max-w-sm"
+              className="max-w-sm rounded"
               src="https://github.com/Viniciusmbc/enterneint-web-next/blob/main/public/thumbnails/beyond-earth/trending/large.jpg?raw=true"
               alt="beyond-earth"
             />
