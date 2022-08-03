986ed198e30c36dea4374b0692c294e7b1b26f7b
Viniciusmbc <viniciuscaceres@gmail.com>
2022-08-03 18:18:47 -0300

    feat/middleware

--- middleware.js
+++ middleware.js
@@ -1,29 +1,29 @@
-
 // <root>/middleware.ts
-import { redirect } from 'next/dist/server/api-utils';
+import { redirect } from "next/dist/server/api-utils";
 // eslint-disable-next-line @next/next/no-server-import-in-page
-import { NextRequest, NextResponse } from 'next/server'
-import { supabase } from './utils/supabaseClient';
+import { NextResponse } from "next/server";
+import { supabase } from "./utils/supabaseClient";
 
-export async function middleware(request: NextRequest) {
-  const  user  = await supabase.auth.api.getUserByCookie(request);
-  const cookies = request.cookies.entries().next()
-  const sbtoken = cookies?.value[1] 
-    if(request.nextUrl.pathname === '/') {
+export async function middleware(request) {
+  const user = await supabase.auth.api.getUserByCookie(request);
+  const cookies = request.cookies.entries().next();
+  console.log(cookies);
+  if (request.nextUrl.pathname === "/") {
+    if (cookies.value !== undefined) {
+      const sbtoken = cookies?.value[1];
+
       if (sbtoken) {
-        return NextResponse.rewrite(new URL('/dashboard', request.url))
+        return NextResponse.rewrite(new URL("/dashboard", request.url));
       }
-    
     }
 
-  if (request.nextUrl.pathname.startsWith('/dashboard')) {
-    if (sbtoken) {
-      const url = request.nextUrl.clone()
-      console.log(url)
-      return NextResponse.rewrite(url)
+    if (request.nextUrl.pathname.startsWith("/dashboard")) {
+      if (sbtoken) {
+        const url = request.nextUrl.clone();
+        console.log(url);
+        return NextResponse.rewrite(url);
+      }
     }
-
-    return NextResponse.rewrite(new URL('/login', request.url))
-}
-
+    return NextResponse.rewrite(new URL("/login", request.url));
+  }
 }
